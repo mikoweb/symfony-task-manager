3b4b0749c31fffca9bfb438a5909152a3a0a4651
Rafał Mikołajun <root@rmweb.pl>
2026-03-02 19:29:18 +0100

    feat(user): add user authentication

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
