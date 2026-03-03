c4ce95047e1677d114f49bc99cb30972b877aae0
Rafał Mikołajun <root@rmweb.pl>
2026-03-03 20:38:38 +0100

    feat(user): add api docs for logged-in user data endpoint

--- src/Application/User/Controller/LoggedUserController.php
+++ src/Application/User/Controller/LoggedUserController.php
@@ -7,11 +7,22 @@ use App\Core\Controller\RestController;
 use App\Core\Mapper\ObjectMapper;
 use App\Domain\User\Entity\User;
 use App\Domain\User\UserRoleName;
+use Nelmio\ApiDocBundle\Attribute\Model;
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class LoggedUserController extends RestController
 {
+    #[OA\Tag(name: 'User')]
+    #[OA\Response(
+        response: 200,
+        description: 'Logged-in user data',
+        content: new OA\JsonContent(
+            ref: new Model(type: UserDto::class),
+            type: 'object',
+        )
+    )]
     #[IsGranted(UserRoleName::ROLE_USER)]
     public function getUserData(ObjectMapper $objectMapper): Response
     {
