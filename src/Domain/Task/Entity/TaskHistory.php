de9a1ed64e5beef6853879c4a932dcf4402b1093
Rafał Mikołajun <root@rmweb.pl>
2026-03-04 14:00:16 +0100

    fix: relation in TaskHistory

--- src/Domain/Task/Entity/TaskHistory.php
+++ src/Domain/Task/Entity/TaskHistory.php
@@ -3,25 +3,24 @@
 namespace App\Domain\Task\Entity;
 
 use App\Core\Doctrine\Entity\History;
-use App\Domain\User\Entity\User;
 use App\Infrastructure\Task\Repository\ORMTaskHistoryRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
 
 /**
- * @extends History<User>
+ * @extends History<Task>
  *
- * @method User getRelatedObject()
+ * @method Task getRelatedObject()
  */
 #[ORM\Entity(repositoryClass: ORMTaskHistoryRepository::class)]
 #[ORM\Table(name: 'task_history')]
 class TaskHistory extends History
 {
     /**
-     * @var User|null
+     * @var Task|null
      */
     #[ORM\JoinColumn(name: 'related_object_id', referencedColumnName: 'id', nullable: true, onDelete: 'SET NULL')]
-    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\ManyToOne(targetEntity: Task::class)]
     protected ?object $relatedObject;
 
     #[ORM\Column(name: 'related_id', type: UuidType::NAME, index: true)]
