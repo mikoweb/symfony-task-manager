34a5c04bff87706ef295a377f37186a39e3ef225
Rafał Mikołajun <root@rmweb.pl>
2026-03-05 22:27:44 +0100

    test(setup): add symfony/test-pack

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
